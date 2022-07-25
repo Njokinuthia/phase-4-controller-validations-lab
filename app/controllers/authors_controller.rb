447645cf58c111cbc8c3f6e1e89c60a74bf307cd
Njoki <kinuthiash@gmail.com>
2022-07-25 09:39:18 +0300

    commit tree 615c90bbdea8d0a2c13e02fc5ceafbb6b710c286

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,6 @@
 class AuthorsController < ApplicationController
-  
+  # rescue_from ActiveRecord::RecordInvalid, with: :render_error_message
+
   def show
     author = Author.find(params[:id])
 
@@ -7,12 +8,18 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    author = Author.create(author_params)
-
+    author = Author.create!(author_params)
     render json: author, status: :created
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
+
   private
+
+  # def render_error_message(invalid)
+  #   render json: { errors: invalid.record.errors.full_messages}, status: :unprocessed_entity
+  # end
   
   def author_params
     params.permit(:email, :name)
